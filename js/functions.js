695ede8a4946f85082321819a68c47888ce503af
AntonPolo <98082279+AntonPolo@users.noreply.github.com>
2022-05-03 19:03:26 +0300

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,42 @@
-function pageContent(Num) {
-    var num = Num;
-console.log(num);
+$("#measuring").on("click", function(){
 
-$.ajax({
-    type: "post",
-    url: "./menu_page/content.php",
-    data: {num: num},
-    dataType: "text",
-    success: function (response) {
-        if (!response) {
-           console.log('not')
-        }else {
-            $("#pp").html(response)
-        }
+    let measuring_form_name = $('input[name="measuring_form_name"]').val();
+
+    let measuring_form_phone = $('input[name="measuring_form_phone"]').val();
+
+    if(measuring_form_name == "" || measuring_form_phone == ""){
+
+        alert("Вы заполнили не все поля!");
+
+    }else{
+
+    
+        $.ajax({
+            url: "./mail.php",
+            type: "POST",
+            cache: false,
+            data: {
+
+                measuring_form_name: measuring_form_name,
+                measuring_form_phone: measuring_form_phone
+
+            },
+            beforeSend: function () {
+                $(".measuring").prop("disabled", true);
+                $(".btn-primary").css("background-color", "#a09c9c");
+            },
+            success: function (data) {
+                if (!data) {
+                    alert("Операция не выполнена, попробуйте позже.");
+                } else {
+                    $(".measuring").prop("disabled", true);
+                    $(".btn-primary").css("background-color", "#0d6efd");
+                    //alert(data);
+                    alert("Сообщение отправленно, ожидайте звонка на указанный номер.");
+
+                }
+                $(".measuring").prop("disabled", false);
+            }
+        })
     }
-});
-}
+})
